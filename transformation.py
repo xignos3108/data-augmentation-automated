0c391ee15d7c6fc71683df6021a0539bed5b6918
xignos3108 <67992618+xignos3108@users.noreply.github.com>
2021-12-13 18:33:06 +0900

    first_commit

--- transformation.py
+++ transformation.py
@@ -2,120 +2,29 @@ import albumentations as A
 import cv2
 import os.path
 
+from configs import PIP_CONFIGS
 
 class transformation():
     def __init__(self, file_path):
         self.current_path = file_path
         self.load_img_path = os.path.join(self.current_path, "dataset") 
         self.load_label_path = os.path.join(self.current_path, "dataset") 
-        self.save_img_path = os.path.join(self.current_path, "dataset_aug") 
+        self.save_img_path = os.path.join(self.current_path, "dataset_augmented") 
         if os.path.exists(self.save_img_path) == False:
             os.mkdir(self.save_img_path)
-        self.save_label_path = os.path.join(self.current_path, "dataset_aug")
+        self.save_label_path = os.path.join(self.current_path, "dataset_augmented")
         if os.path.exists(self.save_img_path) == False:
             os.mkdir(self.save_img_path)
 
-    '''
-    def get_pixel_tf(self):
-        pixel_tf = [
-            A.RandomBrightnessContrast(p=0.3),
-            A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=0.3),
-            A.Sharpen()
-        ]
-        return pixel_tf
-
-    def get_spacial_tf(self):
-        spacial_tf = [
-            A.HorizontalFlip(p=0.5),
-            A.ShiftScaleRotate(p=0.5),
-        ]
-        return spacial_tf
-        '''
-    def get_pixel_tf(self):
-        pixel_tf = [
-            A.AdvancedBlur(),
-            A.Blur(),
-            A.CLAHE(p=1.0), # suggested
-            A.ChannelDropout(),
-            A.ChannelShuffle(),
-            A.ColorJitter(),
-            A.Downscale(),
-            A.Emboss(),
-            A.Equalize(),
-            #A.FDA(),
-            A.FancyPCA(),
-            A.FromFloat(), # white 10
-            A.GaussNoise(),
-            A.GaussianBlur(),
-            A.GlassBlur(),
-            #A.HistogramMatching(),
-            A.HueSaturationValue(),
-            A.ISONoise(),
-            A.ImageCompression(),
-            A.InvertImg(), # color negative 17
-            A.MedianBlur(),
-            A.MotionBlur(),
-            A.MultiplicativeNoise(),
-            A.Normalize(), # blacked 21
-            #A.PixelDistributionAdaptation(),
-            A.Posterize(),
-            A.RGBShift(),
-            A.RandomBrightnessContrast(),
-            A.RandomFog(),
-            A.RandomGamma(),
-            A.RandomRain(),
-            A.RandomShadow(),
-            A.RandomSnow(),
-            A.RandomSunFlare(),
-            A.RandomToneCurve(),
-            A.RingingOvershoot(),
-            A.Sharpen(p=1.0), # suggested
-            A.Solarize(),
-            A.Superpixels(), # 뭉개짐 35
-            #A.TemplateTransform(),
-            A.ToFloat(), # blacked 36
-            A.ToGray(),
-            A.ToSepia(),
-            A.UnsharpMask()
-        ] # 40
-        return pixel_tf
-        
-    def get_spacial_tf(self):
-        spacial_tf = [
-            A.Affine(),
-            #A.CenterCrop(),
-            #A.CropAndPad(),
-            #A.CropNonEmptyMaskIfExists(),
-            A.HorizontalFlip(),
-            A.Lambda(),
-            A.LongestMaxSize(),
-            A.PadIfNeeded(),
-            A.Perspective(),
-            A.PiecewiseAffine(),
-            #A.RandomCrop(),
-            #A.RandomCropNearBBox(),
-            #A.RandomResizedCrop(),
-            A.RandomRotate90(),
-            A.RandomScale(),
-            #A.RandomSizedCrop(),
-            #A.Resize(),
-            A.Rotate(),
-            A.SafeRotate(),
-            A.ShiftScaleRotate(),
-            A.SmallestMaxSize(),
-            A.Transpose(),
-            A.VerticalFlip()
-        ] # 15
-        return spacial_tf    
-
-    def create_tf_list(self):
+  
+    def get_tf_list(self):
         appended_tf = []
+        configs = PIP_CONFIGS
         
-        pixel_tf = self.get_pixel_tf()
-        spacial_tf = self.get_spacial_tf()
+        pixel_tf = configs['PIXEL_TF'] 
+        spacial_tf = configs['SPACIAL_TF']
         appended_tf = pixel_tf + spacial_tf
         return appended_tf
-
 
     def read_img(self, path):
         img = cv2.imread(path)
@@ -126,7 +35,8 @@ class transformation():
         bboxes_float_list = []
         bbox_list = []
 
-        # label format: (class x1 y1 x2 y2), label example: 0 0.552734 0.373611 0.091406 0.027778
+        # label format: (class x1 y1 x2 y2) 
+        # label example: 0 0.552734 0.373611 0.091406 0.027778
         f = open(path, "r")
         while True:
             line = f.readline()
@@ -148,22 +58,26 @@ class transformation():
         return bboxes_float_list
 
     def do_transform(self, img_path, label_path):
+        configs = PIP_CONFIGS
         save_label_path = self.save_label_path
         save_img_path = self.save_img_path
         
         bboxes = self.read_label(label_path)
         img = self.read_img(img_path)
         
-        
         img_name = img_path[-11:-4]
         label_name = label_path[-11:-4]
         
-        tf_list = self.create_tf_list()
-        
+        tf_default_list = configs['DEFAULT_TF']
+        tf_list = self.get_tf_list()
 
         for idx, tf in enumerate(tf_list):
             # create transformation pipline
-            transform = A.Compose(tf, bbox_params=A.BboxParams(format='yolo'))
+            tf_pipline = []
+            for d_tf in tf_default_list:
+                tf_pipline.append(d_tf)
+            tf_pipline.append(tf)
+            transform = A.Compose(tf_pipline, bbox_params=A.BboxParams(format='yolo'))
             
             # do transform
             transformed = transform(image=img, bboxes=bboxes)
@@ -171,9 +85,6 @@ class transformation():
             # save transformed label data
             #transformed_category_ids = transformed['category_ids']
             transformed_bboxes = transformed['bboxes']
-
-            # transformed_bboxes example: 
-            # [(347.25, 138.95, 147.08999999999997, 164.88), (0.5000000000000004, 80.84, 132.80000000000004, 181.84)]
 
             # transformed_bboxes is 'tuple in list' structure 
             transformed_bboxes_list_list = []
@@ -202,5 +113,4 @@ class transformation():
             transformed_image = transformed['image']
             save_img_dir = os.path.join(save_img_path, img_name+"_"+str(idx)+".jpg")
             cv2.imwrite(save_img_dir, transformed_image)
-
             
