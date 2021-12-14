1379ce29ca315aed629cc0654ea468c9d97508fb
xignos3108 <67992618+xignos3108@users.noreply.github.com>
2021-12-14 17:32:55 +0900

    1.bug fixed 2.existing augmented file checker added

--- configs.py
+++ configs.py
@@ -8,48 +8,48 @@ PIP_CONFIGS = {
     
     'PIXEL_TF': [
         A.AdvancedBlur(p=1.0),
-        A.Blur(p=1.0),
+        #A.Blur(p=1.0),
         A.CLAHE(p=1.0), # suggested
-        A.ChannelDropout(p=1.0),
+        #A.ChannelDropout(p=1.0),
         A.ChannelShuffle(p=1.0),
         A.ColorJitter(p=1.0),
         A.Downscale(p=1.0),
         A.Emboss(p=1.0),
         A.Equalize(p=1.0),
         #A.FDA(),
-        A.FancyPCA(p=1.0),
+        #A.FancyPCA(p=1.0),
         #A.FromFloat(), # white 10
         A.GaussNoise(p=1.0),
-        A.GaussianBlur(p=1.0),
+        #A.GaussianBlur(p=1.0),
         A.GlassBlur(p=1.0),
         #A.HistogramMatching(),
         A.HueSaturationValue(p=1.0),
         A.ISONoise(p=1.0),
-        A.ImageCompression(p=1.0),
-        A.InvertImg(p=1.0), # color negative 17
-        A.MedianBlur(p=1.0),
-        A.MotionBlur(p=1.0),
+        #A.ImageCompression(p=1.0),
+        #A.InvertImg(p=1.0), # color negative 17
+        #A.MedianBlur(p=1.0),
+        #A.MotionBlur(p=1.0),
         A.MultiplicativeNoise(p=1.0), # suggested
         #A.Normalize(), # blacked 21
         #A.PixelDistributionAdaptation(),
-        A.Posterize(p=1.0),
-        A.RGBShift(p=1.0),
+        #A.Posterize(p=1.0),
+        #A.RGBShift(p=1.0),
         A.RandomBrightnessContrast(p=1.0),
-        A.RandomFog(p=1.0),
-        A.RandomGamma(p=1.0),
+        #A.RandomFog(p=1.0),
+        #A.RandomGamma(p=1.0),
         A.RandomRain(p=1.0),
         A.RandomShadow(p=1.0),
-        A.RandomSnow(p=1.0),
-        A.RandomSunFlare(p=1.0),
+        #A.RandomSnow(p=1.0),
+        #A.RandomSunFlare(p=1.0),
         A.RandomToneCurve(p=1.0),
         A.RingingOvershoot(p=1.0),
-        A.Sharpen(p=1.0), # suggested
-        A.Solarize(p=1.0),
+        #A.Sharpen(p=1.0), # suggested
+        #A.Solarize(p=1.0),
         #A.Superpixels(), # 뭉개짐 35
         #A.TemplateTransform(),
-        A.ToFloat(p=1.0), # blacked 36
+        #A.ToFloat(p=1.0), # blacked 36
         A.ToGray(p=1.0),
-        A.ToSepia(p=1.0),
+        #A.ToSepia(p=1.0),
         A.UnsharpMask(p=1.0)
         ], # 40
 
@@ -59,8 +59,8 @@ PIP_CONFIGS = {
         #A.CropAndPad(),
         #A.CropNonEmptyMaskIfExists(),
         A.HorizontalFlip(p=1.0),
-        A.Lambda(p=1.0),
-        A.LongestMaxSize(p=1.0),
+        #A.Lambda(p=1.0),
+        #A.LongestMaxSize(p=1.0),
         A.PadIfNeeded(p=1.0),
         A.Perspective(p=1.0),
         A.PiecewiseAffine(p=1.0),
@@ -72,9 +72,9 @@ PIP_CONFIGS = {
         #A.RandomSizedCrop(),
         #A.Resize(),
         A.Rotate(p=1.0),
-        A.SafeRotate(p=1.0),
-        A.ShiftScaleRotate(p=1.0),
-        A.SmallestMaxSize(p=1.0),
+        #A.SafeRotate(p=1.0),
+        #A.ShiftScaleRotate(p=1.0),
+        #A.SmallestMaxSize(p=1.0),
         A.Transpose(p=1.0),
         A.VerticalFlip(p=1.0)
     ] # 15
--- transformation.py
+++ transformation.py
@@ -54,7 +54,15 @@ class transformation():
                 bbox_float = float(bbox_list[i][j])
                 bboxes_float.append(bbox_float)
             bboxes_float.append(bbox_list[i][4])
-            if 2*bboxes_float[0]-bboxes_float[2] < 0.0:
+            
+            bbox_x1 = 2*bboxes_float[0]-bboxes_float[2]
+            bbox_y1 = 2*bboxes_float[1]-bboxes_float[3]
+            bbox_x2 = 2*bboxes_float[0]+bboxes_float[2]
+            bbox_y2 = 2*bboxes_float[1]+bboxes_float[3]
+            if (bbox_x1<0.0 and bbox_x1>1.0 
+            and bbox_y1<0.0 and bbox_y1>1.0 
+            and bbox_x2<0.0 and bbox_x2>1.0
+            and bbox_y2<0.0 and bbox_y2>1.0):
                 continue
             bboxes_float_list.append(bboxes_float)    
         return bboxes_float_list
@@ -65,56 +73,58 @@ class transformation():
         save_label_path = self.save_label_path
         save_img_path = self.save_img_path
         
+        label_name = label_path[-11:-4] 
+        img_name = img_path[-11:-4]
+        
         bboxes = self.read_label(label_path)
         img = self.read_img(img_path)
-        
-        img_name = img_path[-11:-4]
-        label_name = label_path[-11:-4]
         
         tf_default_list = configs['DEFAULT_TF']
         tf_list = self.get_tf_list()
 
         for idx, tf in enumerate(tf_list):
-            # create transformation pipline
-            tf_pipline = []
-            for d_tf in tf_default_list:
-                tf_pipline.append(d_tf)
-            tf_pipline.append(tf)
-            transform = A.Compose(tf_pipline, bbox_params=A.BboxParams(format='yolo'))
-            
-            # do transform
-            transformed = transform(image=img, bboxes=bboxes)
-
-            # save transformed label data
-            #transformed_category_ids = transformed['category_ids']
-            transformed_bboxes = transformed['bboxes']
-
-            # transformed_bboxes is 'tuple in list' structure 
-            transformed_bboxes_list_list = []
-            for i in range (len(transformed_bboxes)):
-                transformed_bboxes_list = []      
-                transformed_bboxes_list.append(str(transformed_bboxes[i][-1])) # class
-                transformed_bboxes_list.append(str(round(transformed_bboxes[i][0], 6)))
-                transformed_bboxes_list.append(str(round(transformed_bboxes[i][1], 6)))
-                transformed_bboxes_list.append(str(round(transformed_bboxes[i][2], 6)))
-                transformed_bboxes_list.append(str(round(transformed_bboxes[i][3], 6)))
-                transformed_bboxes_list_list.append(transformed_bboxes_list)
-            
-
+            ## check if the file already exists
             save_label_dir = os.path.join(save_label_path, label_name+"_"+str(idx)+".txt")
-            # save_label_path = C:/Dev/Image Augmentation/aug_dataset
-            # label_name = A(0001)
-            # rest = "_idx.txt"
-            f = open(save_label_dir, "w")
-            for i in range(len(transformed_bboxes_list_list)):
-                for j in range(len(transformed_bboxes_list_list[i])):
-                    f.write(transformed_bboxes_list_list[i][j] + " ")
-                f.write("\n")
-            f.close()
-            
-            # save transformed image
-            transformed_image = transformed['image']
-            transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR)
             save_img_dir = os.path.join(save_img_path, img_name+"_"+str(idx)+".jpg")
-            cv2.imwrite(save_img_dir, transformed_image)
-            
+            if os.path.exists(save_label_dir) == True:
+                continue
+            else:
+                ## create transformation pipline
+                tf_pipline = []
+                for d_tf in tf_default_list:
+                    tf_pipline.append(d_tf)
+                tf_pipline.append(tf)
+                transform = A.Compose(tf_pipline, bbox_params=A.BboxParams(format='yolo'))
+                
+                ## do transform
+                transformed = transform(image=img, bboxes=bboxes)
+
+                ## save transformed label data
+                #transformed_category_ids = transformed['category_ids']
+                transformed_bboxes = transformed['bboxes']
+
+                # transformed_bboxes is 'tuple in list' structure 
+                transformed_bboxes_list_list = []
+                for i in range (len(transformed_bboxes)):
+                    transformed_bboxes_list = []      
+                    transformed_bboxes_list.append(str(transformed_bboxes[i][-1])) # class
+                    transformed_bboxes_list.append(str(round(transformed_bboxes[i][0], 6)))
+                    transformed_bboxes_list.append(str(round(transformed_bboxes[i][1], 6)))
+                    transformed_bboxes_list.append(str(round(transformed_bboxes[i][2], 6)))
+                    transformed_bboxes_list.append(str(round(transformed_bboxes[i][3], 6)))
+                    transformed_bboxes_list_list.append(transformed_bboxes_list)
+                
+                # save_label_path = C:/Dev/Image Augmentation/aug_dataset
+                # label_name = A(0001)
+                # rest = "_idx.txt"
+                f = open(save_label_dir, "w")
+                for i in range(len(transformed_bboxes_list_list)):
+                    for j in range(len(transformed_bboxes_list_list[i])):
+                        f.write(transformed_bboxes_list_list[i][j] + " ")
+                    f.write("\n")
+                f.close()
+                
+                ## save transformed image
+                transformed_image = transformed['image']
+                transformed_image = cv2.cvtColor(transformed_image, cv2.COLOR_RGB2BGR)
+                cv2.imwrite(save_img_dir, transformed_image)
